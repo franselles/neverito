db3d7fe28c6e68dee5e39a0f5bac16ac56568f7f
franselles <fran.selles@gmail.com>
2022-02-11 12:38:49 +0100

    Add - Change Item in order

--- src/views/EditOrder.vue
+++ src/views/EditOrder.vue
@@ -8,7 +8,16 @@
   <div class="p-2">
     <form @submit.prevent="onSubmit">
       <div class="input-group mb-3">
-        <span class="input-group-text">MODELO</span>
+        <span class="input-group-text">Elemento</span>
+        <select v-model="currentOrder.itemId._id" class="form-select">
+          <option v-for="item in items" :value="item._id">
+            {{ item.name }}
+          </option>
+        </select>
+      </div>
+
+      <div class="input-group mb-3">
+        <span class="input-group-text">Modelo</span>
         <input
           v-model="currentOrder.model"
           type="text"
@@ -18,7 +27,7 @@
       </div>
 
       <div class="input-group mb-3">
-        <span class="input-group-text">CANTIDAD</span>
+        <span class="input-group-text">Cantidad</span>
         <input
           v-model="currentOrder.quantity"
           type="number"
@@ -37,49 +46,53 @@
   </div>
 </template>
 
-<script>
+<script setup>
 import { ref } from 'vue';
 import { useRouter } from 'vue-router';
 import { orderStore } from '../store/orderStore';
+import { itemStore } from '../store/itemStore';
 
-export default {
-  setup() {
-    const router = useRouter();
-    const order = orderStore();
+const router = useRouter();
+const order = orderStore();
+const item = itemStore();
 
-    const currentOrder = ref({});
+const currentOrder = ref({
+  familyId: Object,
+  itemId: Object,
+  userId: Object,
+  model: '',
+  quantity: 0,
+  buyed: false,
+  dateOrder: '',
+  datePurchase: '',
+});
+const items = ref([]);
 
-    const find = async function () {
-      await order.findOrder();
-      currentOrder.value = order.currentOrder;
-    };
+const find = async function () {
+  await item.getItems();
+  items.value = item.items;
 
-    find();
+  await order.findOrder();
+  currentOrder.value = order.currentOrder;
+};
 
-    const onSubmit = async function () {
-      order.currentOrder = currentOrder.value;
-      await order.putOrders();
-      router.push({ name: 'CurrentOrder' });
-    };
+find();
 
-    const onDelete = async function () {
-      if (currentOrder.value._id != null) {
-        await order.deleteOrder(currentOrder.value);
-      }
-      router.push({ name: 'CurrentOrder' });
-    };
+const onSubmit = async function () {
+  order.currentOrder = currentOrder.value;
+  await order.putOrders();
+  router.push({ name: 'CurrentOrder' });
+};
 
-    const onBack = async function () {
-      router.push({ name: 'CurrentOrder' });
-    };
+const onDelete = async function () {
+  if (currentOrder.value._id != null) {
+    await order.deleteOrder(currentOrder.value);
+  }
+  router.push({ name: 'CurrentOrder' });
+};
 
-    return {
-      currentOrder,
-      onSubmit,
-      onDelete,
-      onBack,
-    };
-  },
+const onBack = async function () {
+  router.push({ name: 'CurrentOrder' });
 };
 </script>
 
